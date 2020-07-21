62f09b35a88dcef0369fc436432720d16a984589
manjoos <68403876+SrSandman@users.noreply.github.com>
2020-07-21 10:15:40 -0700

    hahalol
    lol

---
+++ hahalol.py
@@ -0,0 +1,10 @@
+num = input("Enter a number: ")
+num1 = input("Enter another number: ")
+num2 = input("Enter a third number: ")
+yes = True
+no = 0
+lol = input("Do you want a 4th number?: ")
+lol2 = input("lol you only get 3 numbers you sweaty nerd")
+result = float(num) + float(num1) + float(num2)
+
+print(result)
